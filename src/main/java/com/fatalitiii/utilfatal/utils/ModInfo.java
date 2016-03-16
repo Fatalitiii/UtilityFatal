14a988f033d85ddf4f72de0906971d560879d0b5
Fatalitiii <fatalitiii.twitch@gmail.com>
2016-03-16 18:03:24 +0000

    1.4.0.3 Release

--- src/main/java/com/fatalitiii/utilfatal/utils/ModInfo.java
+++ src/main/java/com/fatalitiii/utilfatal/utils/ModInfo.java
@@ -3,5 +3,5 @@ package com.fatalitiii.utilfatal.utils;
 public class ModInfo {
 	public static final String MOD_ID = "utilfatal";
 	public static final String MOD_NAME = "UtilFatal";
-	public static final String VERSION = "1.4.0.1";	
+	public static final String VERSION = "1.4.0.3";	
 }
